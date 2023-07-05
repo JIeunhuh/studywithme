2fb16d63c7993264d77cba5c18c9f1685ce9be2e
JIeunhuh <entks301@naver.com>
2023-07-05 17:36:49 +0900

    프로그래머스

---
+++ java/javaprogramming_study/src/programmers/Level1/K번째수.java
@@ -0,0 +1,14 @@
+package programmers.Level1;
+
+import java.util.Arrays;
+
+public class K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        Arrays.sort(array);
+        
+        return answer;
+    }
+
+
+}
