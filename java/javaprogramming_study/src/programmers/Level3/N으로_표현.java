1fda91a257a4f2d3e8f2da28a5c8ddb9613426bf
JIeunhuh <entks301@naver.com>
2023-07-05 18:00:40 +0900

    프로그래머스(수정)

---
+++ java/javaprogramming_study/src/programmers/Level3/N으로_표현.java
@@ -0,0 +1,18 @@
+package programmers.Level3;
+
+class Solution {
+    // 1 <= N =< 9
+    public int solution(int N, int number) {
+        int answer = 0;
+        // + , - , x , / 으로 표현할 수 있는 최소한의 answer 
+        
+        return answer;
+    }
+}
+
+public class N으로_표현 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.solution(5, 12);
+    }
+}
