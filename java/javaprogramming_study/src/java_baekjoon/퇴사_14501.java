58ddbf6e2972075d80172d5d5b6fd031ae0e543b
JIeunhuh <entks301@naver.com>
2023-07-12 17:43:18 +0900

    백준

---
+++ java/javaprogramming_study/src/java_baekjoon/퇴사_14501.java
@@ -0,0 +1,17 @@
+package java_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 퇴사_14501 {
+    static int[] schedule() {
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+    }
+}
